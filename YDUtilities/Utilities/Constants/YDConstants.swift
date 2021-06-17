47a2458de5693cd54631e0577e32839a976020e1
Douglas Hennrich <douglashennrich@yourdev.com.br>
2021-06-16 23:30:16 -0300

    Adicionando Constants

---
+++ YDUtilities/Utilities/Constants/YDConstants.swift
@@ -0,0 +1,15 @@
+//
+//  YDConstants.swift
+//  YDUtilities
+//
+//  Created by Douglas Hennrich on 16/06/21.
+//
+
+import UIKit
+
+public enum YDConstants {
+  // Notifications
+  public struct Notification {
+    public static let SpaceyNPSChangeValue = NSNotification.Name("YDSpaceyNPSChangeValue")
+  }
+}
