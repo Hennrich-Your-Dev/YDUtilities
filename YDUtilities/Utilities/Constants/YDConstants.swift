b1fbb1d4c2d2bbf0a918909ec139871fed25e56a
Douglas Hennrich <douglashennrich@yourdev.com.br>
2021-07-06 19:50:37 -0300

    Adicionando novas notificações

--- YDUtilities/Utilities/Constants/YDConstants.swift
+++ YDUtilities/Utilities/Constants/YDConstants.swift
@@ -10,8 +10,17 @@ import UIKit
 public enum YDConstants {
   // Notifications
   public struct Notification {
+    // Spacey
     public static let SpaceyNPSChangeValue = NSNotification.Name("YDSpaceyNPSChangeValue")
-    public static let SpaceyErrorTrigger = NSNotification.Name("SpaceyErrorTrigger")
+    public static let SpaceyErrorTrigger = NSNotification.Name("YDSpaceyErrorTrigger")
+    public static let SpaceyProductAddToCard = NSNotification.Name("YDSpaceyProductAddToCard")
+
+    // Components
+    public static let ComponentsCleanUpReply = NSNotification.Name("YDComponentsCleanUpReply")
+    public static let ComponentsMessageFieldResign = NSNotification.Name("YDMessageFieldResignFirstResponder")
+
+    // Miscellaneous
+    public static let Deinit = NSNotification.Name("YDDeinit")
   }
 
   // UserDefaults
